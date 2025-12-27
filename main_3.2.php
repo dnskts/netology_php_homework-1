d3b646f3da4ee95aead594913213b23a199f8ba5
dnskts <dnskts@mail.ru>
2025-12-27 20:06:32 +0300

    Create main_3.2.php

---
+++ main_3.2.php
@@ -0,0 +1,24 @@
+<?php
+$variable = 3.14;
+
+switch (true) {
+    case is_bool($variable):
+        $type = 'bool';
+        break;
+    case is_float($variable):
+        $type = 'float';
+        break;
+    case is_int($variable):
+        $type = 'int';
+        break;
+    case is_string($variable):
+        $type = 'string';
+        break;
+    case is_null($variable):
+        $type = 'null';
+        break;
+    default:
+        $type = 'other';
+}
+
+echo "type is $type";
